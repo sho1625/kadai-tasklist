4c1378325ee4512af0231f5286e294f1a9ec1cac
sho.onitsuka <fber.shoggk1625@gmail.com>
2019-10-22 17:21:52 +0000

    validation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
   
   def create
     @task = Task.new(task_params)
-
+    
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
@@ -29,13 +29,12 @@ class TasksController < ApplicationController
   end
 
   def update
-    
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
     else
       flash.now[:danger] = 'Task は更新されませんでした'
-      render :edit
+      render :new
     end
   end
 
