0d2b60ca0a2a020fa6dff95c02a8f7034ea8ffc4
sho.onitsuka <fber.shoggk1625@gmail.com>
2019-10-22 06:11:16 +0000

    CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,6 +28,8 @@ class TasksController < ApplicationController
   end
 
   def update
+    @task = Task.find(params[:id])
+    
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -49,7 +51,7 @@ class TasksController < ApplicationController
 
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:task)
+    params.require(:task).permit(:content)
   end
 
 end
